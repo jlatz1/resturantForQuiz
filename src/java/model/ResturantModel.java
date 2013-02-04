7866c48038b8b5b38419e5ed914a2f4de1833444
Jessie <Jessie@10.30.16.107>
2013-02-04 12:53:32 -0600

    first commit

---
+++ src/java/model/ResturantModel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.ArrayList;
+import resturant.Resturant;
+
+/**
+ *
+ * @author Jessie
+ */
+public class ResturantModel {
+    private ArrayList<Resturant> orders = new ArrayList<Resturant>();
+    private String [] foods;
+    
+    public ResturantModel(String[] foods){
+        this.foods = foods;
+}
+       public ArrayList<Resturant> processOrders() {
+        for(int x = 0; x < foods.length; x++) { 
+            Resturant r = new Resturant();
+            orders.add(r);
+        }
+        return orders;
+    }
+    
+}
